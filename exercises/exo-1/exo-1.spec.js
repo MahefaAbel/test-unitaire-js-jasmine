809716d4c19945fa0bbbbb44c2ee8b72355d36b1
Mahefa Abel <abelmahefa@gmail.com>
2021-08-31 15:27:40 +0300

    Add more assertion into the test

--- exercises/exo-1/exo-1.spec.js
+++ exercises/exo-1/exo-1.spec.js
@@ -2,13 +2,20 @@
 const exo1 = require("./exo-1");
 
 describe("Nombre", () => {
+    it("devrait etre un nombre est pair", () => {
+        const result = exo1.isOdd(2);
+        expect(result).toBeTrue();
+    });
+    it("devrait etre un nombre est pair", () => {
+        const result = exo1.isOdd(12);
+        expect(result).toBeTrue();
+    });
     it("devrait etre un nombre est impair", () => {
-        const result = exo1.isOdd(12345);
+        const result = exo1.isOdd(1);
         expect(result).toBeFalse();
     });
-    
-    it("devrait etre un nombre est pair", () => {
-        const result = exo1.isOdd(1234);
-        expect(result).toBeTrue();
+    it("devrait etre un nombre est impair", () => {
+        const result = exo1.isOdd(19);
+        expect(result).toBeFalse();
     });
 });
